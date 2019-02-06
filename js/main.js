c24dd31acd19e81ff79a9c970029571e2e0fdff9
Diego Antonio <diegogomez211990@gmail.com>
2019-02-05 20:34:23 -0800

    Partial validation implemented

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
 
-let description = $('#description')
-let calories = $('#calories')
-let carbs = $('#carbs')
-let protein = $('#protein')
+const description = document.getElementById('description')
+const calories = document.getElementById('calories')
+const carbs = document.getElementById('carbs')
+const protein = document.getElementById('protein')
+
+const isValid = elem => elem.value ? '' : elem.classList.add('is-invalid')
+
+const validateInputs = () => {
+  isValid(description)
+  isValid(calories)
+  isValid(carbs)
+  isValid(protein)  
+}
